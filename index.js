61f93559a5d5ac9f0cc839ae26f5a5de381be30b
Falasca Gabriele <gabriele.falasca@sourcesense.com>
2018-09-26 14:19:44 +0200

    refinement for the talk

--- index.js
+++ index.js
@@ -1,50 +1,41 @@
+// import modules
 import fs from 'fs';
-import rimraf from 'rimraf';
 import zlib from 'zlib';
 import colors from 'colors';
-// import { Extract } from 'unzipper';
 import { parseString, Builder } from 'xml2js';
 import archiver from 'archiver';
-import { Sink } from 'pipette';
-// import { Parse, Extract } from 'unzip-stream';
 import AdmZip from 'adm-zip';
+import Logo from './logo';
 
-const dir = '.extract';
+// create the read and write streams for read odt file and write the new
 const builder = new Builder();
-const zip = new AdmZip('mimmo.odt');
+const zip = new AdmZip('demo.odt');
 const archive = archiver('zip');
 const odt = fs.createWriteStream('edited.odt');
 archive.pipe(odt);
 
-const logo = `
-  _(_)(_)(_)(_)_ (_)(_)(_)(_) (_)(_)(_)(_)(_)   
-(_)          (_) (_)      (_)_     (_)         
-(_)          (_) (_)        (_)    (_)         
-(_)          (_) (_)        (_)    (_)                         _      
-(_)          (_) (_)       _(_)    (_)            ._ _  ___  _| | ___ 
-(_)_  _  _  _(_) (_)_  _  (_)      (_)            | ' |/ . \/ . |/ ._>
-(_)(_)(_)(_)    (_)(_)(_)(_)       (_)            |_|_|\___/\___|\___.
-`.rainbow;
-
-
-console.log(logo);
-
+console.log(Logo());
 console.log(`${'LIBREOFFICE'.green} conference 2018`);
 
 zip.getEntries().map(entry => {
+  console.log(`Parsing ${entry.entryName}...`.green);
   if (entry.entryName === 'content.xml') {
     parseString(entry.getData().toString('utf8'), (err, json) => {
-      json['office:document-content']['office:body'][0]['office:text'][0]['text:h'][0]['_'] = 'Un nuovo Titolo';
+      // We can use Javascript Array destructuring here
+      // Changing title...
+      json['office:document-content']['office:body'][0]['office:text'][0]['text:h'][0]['_'] = 'A new Title';
+      // Adding a subtitle
       json['office:document-content']['office:body'][0]['office:text'][0]['text:h'][1] = {
-        '_': 'Sottotitolo',
+        '_': 'Subtitle',
         $: {
           'text:style-name': 'P2',
           'text:outline-level': '1',
         }
       };
+      // Adding a Paragraph
       json['office:document-content']['office:body'][0]['office:text'][0]['text:p'] = [
         {
-          '_': 'Paragrafo',
+          '_': 'Paragraph text: blah blah blah',
           $: {
             'text:style-name': 'P2',
             'text:outline-level': '1',
@@ -60,5 +51,6 @@ zip.getEntries().map(entry => {
     archive.append(Buffer.from(entry.getData().toString('utf8')), { name: entry.entryName });
   }
 });
-
+console.log('all files has been parsed, building new odt...');
 archive.finalize();
+console.log('new odt file has been created! ./edited.odt'.green);
